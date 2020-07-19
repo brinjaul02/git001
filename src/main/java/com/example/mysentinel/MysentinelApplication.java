d53df259891d95d134b6f0a4c0065f645f6ca402
brinjaul02 <463232683@qq.com>
2020-07-20 07:13:29 +0800

    www

---
+++ src/main/java/com/example/mysentinel/MysentinelApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysentinelApplication.class, args);
+    }
+
+}
